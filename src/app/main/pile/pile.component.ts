24e412d0f80de004c63370f46dc7d7b5355da1fc
Christian Katzorke <c.katzorke@web.de>
2018-01-03 17:57:52 +0100

    typo

--- src/app/main/pile/pile.component.ts
+++ src/app/main/pile/pile.component.ts
@@ -19,4 +19,31 @@ export class PileComponent implements OnInit {
     });
   }
 
+  showGameplayTime() {
+    const main = this.sumMain();
+    const complete = this.sumCompletionist();
+
+    if (main === 0 && complete === 0) {
+      return 'Sorry, no time information available';
+    } else if (main === 0) {
+      return `It will take you up to <strong>${complete}</strong> hours to beat this pile`;
+    } else if (complete === 0) {
+      return `It will take you up to <strong>${main}</strong> hours to beat this pile`;
+    }
+    return `It will take you from <strong>${main}</strong> to
+      <strong>${complete}</strong> hours to beat this pile`;
+  }
+
+  sumMain(): number {
+    return this.entries
+      .map((e) => e.gameplayMain)
+      .reduce((prev, curr) => prev + curr);
+  }
+
+  sumCompletionist(): number {
+    return this.entries
+      .map((e) => e.gameplayCompletionist)
+      .reduce((prev, curr) => prev + curr);
+  }
+
 }

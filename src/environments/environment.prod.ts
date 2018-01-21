974344db700dc512b5f46e3567e6a15420052516
Christian Katzorke <c.katzorke@web.de>
2018-01-21 21:49:43 +0100

    add firebase for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBUVKVrviQcGO7Po1cb16X538jufZ-o9dA',
+    authDomain: 'piletron3000.firebaseapp.com',
+    databaseURL: 'https://piletron3000.firebaseio.com',
+    projectId: 'piletron3000',
+    storageBucket: 'piletron3000.appspot.com',
+    messagingSenderId: '302651587220'
+  }
 };

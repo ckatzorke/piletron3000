f98e283c30a0bb4de859122f7f2388debce57414
ckatzorke <christian.katzorke@allianz.de>
2018-02-14 08:05:58 +0100

    fix issue #2

--- src/app/main/pile/pile.model.ts
+++ src/app/main/pile/pile.model.ts
@@ -5,5 +5,14 @@ export class PileEntry {
     public imageUrl: string,
     public gameplayMain: number,
     public gameplayCompletionist: number
-  ) { }
+  ) {
+    if (gameplayMain === 0 && gameplayCompletionist !== 0) {
+      console.log('No main play time, setting to ' + gameplayCompletionist);
+      this.gameplayMain = gameplayCompletionist;
+    }
+    if (gameplayMain !== 0 && gameplayCompletionist === 0) {
+      console.log('No completionist play time, setting to ' + gameplayMain);
+      this.gameplayCompletionist = gameplayMain;
+    }
+  }
 }
